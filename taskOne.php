3fa3d2c9766bc749842e3fafc5df578a64237a7d
Awwal <awwal1999@gmail.com>
2018-04-09 11:06:37 +0100

    First commit

---
+++ taskOne.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Stage One</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+</head>
+<body>
+    <div>
+    <h2>Howdy!</h2>
+    <p class="timeFormat"> HH : MM : SS</p>
+    <p class="time"><?php echo date("H:i:s");?>
+        <!-- <span style="color: #63F24C">21 </span>
+        <span style="color: #EB5757">: 10</span>
+        <span style="color: #F24CD7"> : 12</span></p> -->
+    <p class="month">April</p>
+    <p class="day">07</p>
+    <p class="year">2018</p>
+    </div>
+</body>
+</html>
